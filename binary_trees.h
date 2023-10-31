f8f72134d17c207ad6aa134ab4277733fb7ded6b
ekarkat <ekarkat@gmail.com>
2023-10-31 16:18:49 +0000

    task 5

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,8 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 /*Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
+/*print function*/
+void binary_tree_print(const binary_tree_t *);
 
 /*Mandatory functions*/
 
@@ -37,6 +39,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 void binary_tree_delete(binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
-void binary_tree_print(const binary_tree_t *);
+
+
+
 #endif
