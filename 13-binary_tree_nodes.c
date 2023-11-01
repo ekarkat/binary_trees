32533cdb3f7620ffd37f85a352717bf37807a2d2
ekarkat <ekarkat@gmail.com>
2023-11-01 02:23:51 +0000

    task 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,43 @@
+#include "binary_trees.h"
+
+
+/**
+ * count - return 1 if the node has at least 1 child nide else 0
+ * @tree: the root
+ * Return: return 1 if the node has at least 1 child nide else 0
+*/
+
+size_t count(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (0);
+	return (1);
+}
+
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: Height of the tree, or 0 if tree is NULL
+*/
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+
+	int left = 0;
+	int right = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (0);
+
+
+	left = count(tree) + binary_tree_nodes(tree->left);
+	right = right + binary_tree_nodes(tree->right);
+
+
+	return ((left + right));
+}
