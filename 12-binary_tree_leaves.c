aa20e0ba3322cca0b58e942fe34bda7919b7cf14
ekarkat <ekarkat@gmail.com>
2023-11-01 01:58:45 +0000

    task 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: Pointer to the root node of the tree to measure the height
+ * Return: Height of the tree, or 0 if tree is NULL
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+
+	int left = 0;
+	int right = 0;
+
+	if (!tree)
+		return (0);
+
+
+
+	left = left + binary_tree_leaves(tree->left);
+	right = right + binary_tree_leaves(tree->right);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return ((left + right));
+}
