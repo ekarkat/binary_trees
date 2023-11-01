b97f3628022ba22b4b6dcdf473c7d5031f9745c1
ekarkat <ekarkat@gmail.com>
2023-11-01 01:09:15 +0000

    task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,13 +14,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int right = 0;
 
 	if (!tree)
-		return (-1);
+		return (0);
 
 
-	left = 1 + binary_tree_height(tree->left);
+	left = (1 + binary_tree_height(tree->left)) ;
+	right = (1 + binary_tree_height(tree->right)) ;
 
-	right = 1 + binary_tree_height(tree->right);
-
+	if (!tree->left && !tree->right)
+		return (0);
 	if (left <= right)
 		max = right;
 	if (right < left)
