0090112b6b46e06a6b18c23931ba5d65f95b8e77
ekarkat <ekarkat@gmail.com>
2023-11-01 17:34:32 +0000

    tazsk 17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: the node
+ * Return: return the sibling
+*/
+
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node)
+		return (NULL);
+	if (node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
