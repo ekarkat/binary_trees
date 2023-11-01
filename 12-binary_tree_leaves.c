894002fe8d9d10934568ebca201567049b25520d
ekarkat <ekarkat@gmail.com>
2023-11-01 16:14:19 +0000

    task 15

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -15,8 +15,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-
-
 	left = left + binary_tree_leaves(tree->left);
 	right = right + binary_tree_leaves(tree->right);
 
--- 15-main.c
+++ 15-main.c
@@ -24,7 +24,7 @@ int main(void)
     printf("Is %d full: %d\n", root->n, full);
     full = binary_tree_is_full(root->left);
     printf("Is %d full: %d\n", root->left->n, full);
-    full = binary_tree_is_full(root->right);
-    printf("Is %d full: %d\n", root->right->n, full);
+    full = binary_tree_is_full(root->right->right);
+    printf("Is %d full: %d\n", root->right->right->n, full);
     return (0);
 }
