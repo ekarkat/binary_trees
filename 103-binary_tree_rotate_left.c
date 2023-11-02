d5ab47c12fd79e9c838215f6a5f29fe1778d407b
ekarkat <ekarkat@gmail.com>
2023-11-02 22:26:57 +0000

    rask 1003

---
+++ 103-binary_tree_rotate_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_rotate_left - Rotates a binary tree to the left
+ * @tree: Pointer to the root node of the tree to rotate
+ * Return: Pointer to the new root node of the rotated tree
+*/
+
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+{
+	binary_tree_t *right;
+	binary_tree_t *par;
+
+
+	if (!tree)
+		return (NULL);
+
+	if (!tree->right)
+		return (tree);
+	right = tree->right;
+	par = tree->parent;
+
+
+	tree->parent = right;
+	tree->right = right->left;
+
+	right->left = tree;
+	right->parent = par;
+
+	return (right);
+}
